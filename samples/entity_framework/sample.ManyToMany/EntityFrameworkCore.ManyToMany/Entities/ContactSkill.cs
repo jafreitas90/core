b028664efaaf9655cb501c2102123c1b95e0a6b8
Jorge Freitas <jorge.freitas@wolterskluwer.com>
2020-10-24 18:19:14 +0100

    Entity Framework Many-to-Many relationship

---
+++ samples/entity_framework/sample.ManyToMany/EntityFrameworkCore.ManyToMany/Entities/ContactSkill.cs
@@ -0,0 +1,39 @@
+﻿using EntityFrameworkCore.ManyToMany.Entities.Enums;
+
+namespace EntityFrameworkCore.ManyToMany.Entities
+{
+    public class ContactSkill
+    {
+        public int ContactId { get; set; }
+        public Contact Contact { get; set; }
+
+        public int SkillId { get; set; }
+        public Skill Skill { get; set; }
+
+        public ExpertiseLevel Level { get; set; }
+
+        private ContactSkill()
+        {
+        }
+
+        public ContactSkill(ExpertiseLevel expertiseLevel, int skillId)
+        {
+            Level = expertiseLevel;
+            SkillId = skillId;
+        }
+
+        public ContactSkill(ExpertiseLevel expertiseLevel, int skillId, int contactId)
+        {
+            Level = expertiseLevel;
+            SkillId = skillId;
+            ContactId = contactId;
+        }
+
+        public ContactSkill(ExpertiseLevel expertiseLevel, Skill skill, Contact contact)
+        {
+            Level = expertiseLevel;
+            Skill = skill;
+            Contact = contact;
+        }
+    }
+}
